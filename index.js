17d7a74c2750e3803d213282eee90bb2d4467002
avneetkaur17 <kauravneet2082@gmail.com>
2025-06-16 21:47:49 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function openInstagram(){
+    window.open("https://www.instagram.com/nailsbyavni?igsh=aHpuZTNzY3RjY3Bx", "_blank");
+}
+
+function openPinterest(){
+    window.open("https://pin.it/4hgQCpI7k", "_blank");
+}
